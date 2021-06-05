f9e35f4706b4ca8687a3c921eac18b6393e7cf6e
Erick Alexander Rangel Urbina <1719110247@utectulancingo.edu.mx>
2021-06-05 00:27:57 -0500

    like usual

---
+++ project_01/bd_basics_app/registro_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.manager import Manager
+
+# Create your models here.
+class grupo(models.Model):
+    tittle =  models.CharField(max_length=50)
+
+class alumno(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido_paterno = models.CharField(max_length=50)
+    apellido_materno = models.CharField(max_length=50)
+    no_control = models.CharField(max_length=10)
+    cuatrimestres = models.CharField(max_length=5)
+    grupo = models.ForeignKey(grupo, on_delete=models.CASCADE)
---
+++ project_01/bd_basics_app/registro_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def listar_alumnos(request):
+    return
+    
+def form_alumnos(request):
+    return
+
+def borrar_alumnos(request):
+    return
